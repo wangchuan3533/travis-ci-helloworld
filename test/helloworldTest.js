1bef2df3c634f129b69628ab5c89ded3c46e6a83
wangchuan <wangchuan3533@gmail.com>
2015-09-17 12:21:08 +0800

    test

---
+++ test/helloworldTest.js
@@ -0,0 +1,9 @@
+var should = require('should');
+var helloworld = require('../src/helloworld');
+describe('helloworld', function () {
+  it('should equal', function () {
+    helloworld.should.equal('helloworld');
+  });
+});
+    
+    
